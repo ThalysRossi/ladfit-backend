f9d9b2efc97c79f0066b5574e0c475043c20950a
Thalys Rossi <thalys.g.rossi@gmail.com>
2020-12-23 16:20:20 -0300

    Fixed routes

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const Router = require('express').Router;
 
 const UsuarioController = require('./app/controllers/UsuarioController');
 const SessionController = require('./app/controllers/SessionController');
+const ExercicioController = require('./app/controllers/ExercicioController');
 
 const authMiddleware = require('./app/middlewares/auth');
 
@@ -9,12 +10,15 @@ const routes = new Router();
 
 routes.post('/usuario', UsuarioController.store);
 routes.post('/sessions', SessionController.store);
+routes.post('/exercicios', ExercicioController.store);
 
 routes.use(authMiddleware);
 routes.put('/usuario', UsuarioController.update);
+routes.put('/exercicios', ExercicioController.update);
 routes.get('/usuario', UsuarioController.list);
 routes.get('/usuario/:id', UsuarioController.listOne);
-
+routes.get('/exercicios/:id', ExercicioController.listOne);
+routes.get('/exercicios', ExercicioController.list);
 
 
 module.exports = routes;
